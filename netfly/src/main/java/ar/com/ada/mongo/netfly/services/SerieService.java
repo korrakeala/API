d7d7778fea7b9575199a8c7b7361809eef4c98c2
korrakeala <renata.mansilla@gmail.com>
2019-11-20 11:25:27 -0300

    nuevo user mongoDB con menos permisos

--- netfly/src/main/java/ar/com/ada/mongo/netfly/services/SerieService.java
+++ netfly/src/main/java/ar/com/ada/mongo/netfly/services/SerieService.java
@@ -92,4 +92,48 @@ public class SerieService {
         return e;
     }
 
+    public enum SerieValidationType {
+        
+        SERIE_OK,
+        TEMPORADAS_NULA, 
+        TEMPORADAS_VACIA, 
+        TEMPORADA_DUPLICADA, 
+        TEMPORADA_INVALIDA,
+
+        SERIE_DATOS_INVALIDOS 
+
+    }
+
+    public SerieValidationType verificarSerie(Serie serie) {
+
+        if (serie.getNombre() == null)
+            return SerieValidationType.SERIE_DATOS_INVALIDOS;
+
+        if (serie.getAnio() <= 0)
+            return SerieValidationType.SERIE_DATOS_INVALIDOS;
+
+        if (serie.getTemporadas() == null)
+            return SerieValidationType.TEMPORADAS_NULA;
+
+        if (serie.getTemporadas().size() == 0)
+            return SerieValidationType.TEMPORADAS_VACIA;
+
+        //Armo un hashmap para ver si la temporada esta duplicada
+        HashMap<Integer, Temporada> unicasTemps = new HashMap<>();
+
+        for (Temporada t : serie.getTemporadas()) {
+            if (unicasTemps.containsKey(new Integer(t.getNroTemporada())))
+                return SerieValidationType.TEMPORADA_DUPLICADA;
+                
+            if (t.getEpisodios().size() == 0)
+                return SerieValidationType.TEMPORADA_INVALIDA;
+        
+        unicasTemps.put(new Integer(t.getNroTemporada()), t);
+        
+            }
+
+        return SerieValidationType.SERIE_OK;
+    }
+
+
 }
